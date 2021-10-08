19e3fd7c7d795f201bf45a3e04344b4acf68c176
Vinayselukar21 <vselukar2102@gmail.com>
2021-10-08 18:06:37 +0530

    added animations

---
+++ index.js
@@ -0,0 +1,11 @@
+// <!--Initiate WOW js, can be at the bottom of your file -->
+wow = new WOW(
+    {
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // change this if you are not using animate.css
+        offset:       0,          // default
+        mobile:       true,       // keep it on mobile
+        live:         true        // track if element updates
+      }
+    )
+   wow.init();
